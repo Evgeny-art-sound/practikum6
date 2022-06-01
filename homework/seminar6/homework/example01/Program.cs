d3d8d1e58d97929d40bbfabcb881af0878d869ee
Евгений Шараборин <evgenijsaraborin@MacBook-Air-Evgenij.local>
2022-06-01 19:10:11 +0500

    add tasks

---
+++ homework/seminar6/homework/example01/Program.cs
@@ -0,0 +1,37 @@
+﻿// Показать двумерный массив размером mXn заполненный вещественными числами
+Console.Clear();
+void FullArray(double[,] array)
+{
+    Random rnd = new Random();
+    int rowsLength = array.GetLength(0);
+    int columnsLength = array.GetLength(1);
+    for (int i = 0; i < rowsLength; i++)
+    {
+        for (int j = 0; j < columnsLength; j++)
+        {
+            array[i, j] = rnd.NextDouble();
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    int rowsLength = array.GetLength(0);
+    int columnsLength = array.GetLength(1);
+    for (int i = 0; i < rowsLength; i++)
+    {
+        for (int j = 0; j < columnsLength; j++)
+        {
+            Console.Write(" {0:F8} ", array[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+double[,] array = new double[rows, columns];
+FullArray(array);
+PrintArray(array);
